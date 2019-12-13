fc181cd5f676f06ae965f6b00c1917c40d9a541f
Tangming <infly123@sina.com>
2019-12-13 16:50:17 +0800

    update

--- src/3ParMonitoring/Program.cs
+++ src/3ParMonitoring/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,8 +9,41 @@ namespace _3ParMonitoring
 {
     class Program
     {
+
+
         static void Main(string[] args)
         {
+            // 解决WebClient不能通过https下载内容问题
+            ServicePointManager.ServerCertificateValidationCallback +=
+                delegate (object sender, System.Security.Cryptography.X509Certificates.X509Certificate certificate,
+                 System.Security.Cryptography.X509Certificates.X509Chain chain,
+                 System.Net.Security.SslPolicyErrors sslPolicyErrors)
+                {
+                    return true; // **** Always accept
+                };
+
+            using (WebClient client = new WebClient())
+            {
+                string address = "https://192.168.128.151:8080/api/v1/";
+                client.Encoding = Encoding.UTF8;
+                client.Headers.Add("Content-Type", "application/json; charset=utf-8");
+                client.DownloadStringCompleted += Client_DownloadStringCompleted;
+                client.DownloadStringAsync(new Uri(address));
+            }
+
+            Console.ReadKey();
+        }
+
+        private static void Client_DownloadStringCompleted(object sender, DownloadStringCompletedEventArgs e)
+        {
+            if (e.Error != null)
+            {
+                return;
+            }
+            else
+            {
+                string a = e.Result;
+            }
         }
     }
 }
---
+++ src/3ParMonitoring/WebClientManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ParMonitoring
+{
+    class WebClientManager
+    {
+    }
+
+    public class ParWebClient : WebClient
+    {
+        private int timeOut;
+        public ParWebClient()
+        {
+            this.timeOut = int.Parse(ConfigurationManager.AppSettings["webClientTimeOut"]);
+            this.Encoding = Encoding.UTF8;
+        }
+
+        protected override WebRequest GetWebRequest(Uri address)
+        {
+            HttpWebRequest request = (HttpWebRequest)base.GetWebRequest(address);
+            request.Timeout = timeOut;
+            request.ReadWriteTimeout = timeOut;
+            return request;
+        }
+    }
+}
