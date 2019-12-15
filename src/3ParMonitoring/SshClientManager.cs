a570d43603cbd26ff2ecb23ba052f1aa58bbcd34
TangMing <infly123@sina.com>
2019-12-15 11:10:55 +0800

    Create SshClientManager.cs

---
+++ src/3ParMonitoring/SshClientManager.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3ParMonitoring
+{
+    public class SshClientManager
+    {
+        private string sshHost;
+        private string sshUser;
+        private string sshPassword;
+        private uint sshPort = 22;
+
+        public SshClientManager(string sshHost, string sshUser, string sshPassword)
+        {
+            this.sshHost = sshHost;
+            this.sshUser = sshUser;
+            this.sshPassword = sshPassword;
+        }
+        public SshClientManager(string sshHost, string sshUser, string sshPassword, uint sshPort)
+        {
+            this.sshHost = sshHost;
+            this.sshUser = sshUser;
+            this.sshPassword = sshPassword;
+            this.sshPort = sshPort;
+        }
+    }
+}
