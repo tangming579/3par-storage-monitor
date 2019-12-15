d0131919e1b2c22ecf14c740a18caf7bcb3b3355
TangMing <infly123@sina.com>
2019-12-15 11:24:09 +0800

    Update SshClientManager.cs

--- src/3ParMonitoring/SshClientManager.cs
+++ src/3ParMonitoring/SshClientManager.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Renci.SshNet;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,12 +7,24 @@ using System.Threading.Tasks;
 
 namespace _3ParMonitoring
 {
-    public class SshClientManager
+    public class SshClientManager : IDisposable
     {
         private string sshHost;
         private string sshUser;
         private string sshPassword;
-        private uint sshPort = 22;
+        private int sshPort = 22;
+
+        private SshClient client;
+        private PasswordConnectionInfo connectionInfo;
+
+        public bool IsConnected
+        {
+            get
+            {
+                if (client == null) return false;
+                return client.IsConnected;
+            }
+        }
 
         public SshClientManager(string sshHost, string sshUser, string sshPassword)
         {
@@ -19,12 +32,51 @@ namespace _3ParMonitoring
             this.sshUser = sshUser;
             this.sshPassword = sshPassword;
         }
-        public SshClientManager(string sshHost, string sshUser, string sshPassword, uint sshPort)
+        public SshClientManager(string sshHost, string sshUser, string sshPassword, int sshPort)
         {
             this.sshHost = sshHost;
             this.sshUser = sshUser;
             this.sshPassword = sshPassword;
             this.sshPort = sshPort;
+        }
+
+        public bool Connect()
+        {
+            connectionInfo = new PasswordConnectionInfo(sshHost, sshPort, sshUser, sshPassword);
+            //设置SSH连接超时时间
+            connectionInfo.Timeout = TimeSpan.FromSeconds(10);
+            client = new SshClient(connectionInfo);
+            try
+            {
+                client.Connect();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Can't connect ssh server!");
+                return false;
+            }
+
+            if (!client.IsConnected)
+            {
+                Console.WriteLine("Ssh service unreachable!");
+                return false;
+            }
+            return true;
+        }
+
+        public void Dispose()
+        {
+            if (client != null)
+            {
+                try
+                {
+                    client.Dispose();
+                }
+                catch (Exception exp)
+                {
+                    Console.WriteLine(exp.Message);
+                }
+            }
         }
     }
 }
